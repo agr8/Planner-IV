7e040b141ab24cc30cf3a1e976459eccb55c5fea
José Alberto Gómez García <modej@correo.ugr.es>
2021-10-13 18:51:17 +0200

    Update with add/get methods for Users and Activities - #15

--- src/Handler.ts
+++ src/Handler.ts
@@ -10,4 +10,37 @@ export default class Handler {
         this._users = new Map<string,User>()
         this._activities = new Map<string,Activity>()
     }
+
+    add_user(ID: string, usuario: User) {
+        if(this._users.has(ID)) {
+            return false
+        } else {
+            this._users.set(ID,usuario)
+            return true
+        }
+    }
+    get_user(ID: string) {
+        if (this._users.has(ID))
+            return this._users.get(ID)
+        else
+            return null
+    }
+    //Both add methods check if an User/Activity with the given key exists, as it would be overwritten
+    //Both get methods return null if an User/Activity with a given key doesn't exist
+
+
+    add_activity(ID: string, actividad: Activity) {
+        if(this._activities.has(ID)) {
+            return false
+        } else {
+            this._activities.set(ID,actividad)
+            return true
+        }  
+    }
+    get_activity(ID: string) {
+        if (this._activities.has(ID))
+            return this._activities.get(ID)
+        else
+            return null
+    }
 }
